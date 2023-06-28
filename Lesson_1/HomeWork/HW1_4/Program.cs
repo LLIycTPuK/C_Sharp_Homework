57f624e65ac9596ac19b09cb25982680c1a68eb4
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 19:59:28 +0300

    HomeWork 1_4

---
+++ Lesson_1/HomeWork/HW1_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 0;
+while (count < a)
+{
+    Console.Write(count);
+    count++; // count = count + 1
+}
