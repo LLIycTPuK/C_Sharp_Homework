90843dc105c8459aa18822f2d8b3299fe5f38d5f
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 17:56:59 +0300

    Classwork 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите числа: ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a/b == b)
+{
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
