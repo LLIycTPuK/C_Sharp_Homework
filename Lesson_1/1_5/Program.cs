34d524334a9470e5aeae1b0cb8b58341e123d255
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 18:45:53 +0300

    Classwork 1_5

---
+++ Lesson_1/1_5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int count = -a;
+    Console.WriteLine(count);
+while (count!=a)
+{
+    if (a>0)
+    count++; // count = count + 1
+    else
+    count--; // count = count - 1
+    Console.WriteLine(count);
+}
