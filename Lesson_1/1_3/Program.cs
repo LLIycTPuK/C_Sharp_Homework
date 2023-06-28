d81a304a38c58ca6948a3456ea316bcce874d7dc
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 18:13:50 +0300

    Classwork 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число от 1 до 7: ");
+string a = Console.ReadLine()!;
+switch (a)
+{
+    case "1":
+    Console.WriteLine("Понедельник");
+    break;
+    case "2":
+    Console.WriteLine("Вторник");
+    break;
+    case "3":
+    Console.WriteLine("Среда");
+    break;
+    case "4":
+    Console.WriteLine("Четверг");
+    break;
+    case "5":
+    Console.WriteLine("Пятница");
+    break;
+    case "6":
+    Console.WriteLine("Суббота");
+    break;
+    case "7":
+    Console.WriteLine("Воскресенье");
+    break;
+    default:
+    Console.WriteLine("Такого дня недели нет");
+    break;
+}
