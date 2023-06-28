2df0da03565d59ec955b39182c044cca6a00fd0f
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 19:44:09 +0300

    HomeWork 1_3

---
+++ Lesson_1/HomeWork/HW1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число и узнайте четное оно или нет: ");
+int a = int.Parse(Console.ReadLine()!);
+if ((a % 2) == 0)
+
+    Console.WriteLine("Четное");
+
+else
+
+    Console.WriteLine("Нечентное");
