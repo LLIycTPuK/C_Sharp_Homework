7b5f979c30336cada2c9e34afa0d5743aab3237d
LLIycTPuK <ashliye@yandex.ru>
2023-06-28 17:27:10 +0300

    Classwork 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выдача квадрата входного числа
+
+
+Console.WriteLine("Введите число: ");
+string num_str = Console.ReadLine()!;
+
+//int num = int.Parse(Console.ReadLine());
+int num = int.Parse(num_str);
+
+Console.WriteLine(num * num);
