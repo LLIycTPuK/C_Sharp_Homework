d55ac15aabc810154fbec56a9995fb302dbb6b12
LLIycTPuK <ashliye@yandex.ru>
2023-06-30 15:19:45 +0300

    Classwork 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Max(int num)
+{
+    if (num/10 > num % 10)
+    return num/10;
+
+    return num % 10;
+}
+int Rnum =  new Random().Next(10, 100);
+Console.WriteLine(Rnum);
+int result = Max(Rnum);
+Console.WriteLine(result);
