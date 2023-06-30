ca6c29d17f5fab45823c0749e541c5a955dd43ec
LLIycTPuK <ashliye@yandex.ru>
2023-06-30 15:55:13 +0300

    HomeWork 2_1

---
+++ Lesson_2/HomeWork2/HW2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//123 % 10 -> 3
+//123 % 100 ->23
+
+// 123 / 10 ->12
+//123 / 100 ->1
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 100;
+
+}
+
+
+
+int twolast_num = new Random().Next(100, 1000);
+int result = TakeNum(twolast_num);
+Console.WriteLine(result / 10);
