2f8a856387e1a0eb1814c90758a41593c4ba9358
LLIycTPuK <ashliye@yandex.ru>
2023-07-02 13:18:50 +0300

    HomeWork 2_3 fixx

--- Lesson_2/HomeWork2/HW2_3/Program.cs
+++ Lesson_2/HomeWork2/HW2_3/Program.cs
@@ -3,26 +3,28 @@
 
 // 123 / 10 ->12
 //123 / 100 ->1
-Console.WriteLine("ВВедите любое число и увидите третью цифру этого число: ");
+Console.WriteLine("ВВедите любое число и увидите третью цифру этого числа: ");
 int TakeNum(int num)
 {
-    int result = -1
-    if (num > 99 && num < 1000)
-       int result = num % 10;
-     while (num >= 1000)
+    int result = -1;
+    if (num > 1000)
     {
 
-         num =num / 10
-    }
-        result = number%10;
+        while (num >= 1000)
+        {
 
-    else
-        result = -1;
+            num = num / 10;
+        }
+        result = num % 10;
+    }
+    return result;
 }
+
 
 
 int thd_num = int.Parse(Console.ReadLine()!);
 TakeNum(thd_num);
 if (TakeNum(thd_num) == -1)
-Console.WriteLine("третьей цифры нет");
-else Console.WriteLine(TakeNum(thd_num); 
+    Console.WriteLine("третьей цифры нет");
+else
+    Console.WriteLine($"{TakeNum(thd_num)}");
