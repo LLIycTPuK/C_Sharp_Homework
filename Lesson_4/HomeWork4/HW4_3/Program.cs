a55a90271f3091422ab6b2d2ad3b26bcddb1675d
LLIycTPuK <ashliye@yandex.ru>
2023-07-09 16:00:35 +0300

    HomeWork 4_3

---
+++ Lesson_4/HomeWork4/HW4_3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите диапазон рандомных чисел, max: ");
+int max= int.Parse(Console.ReadLine()!);
+Console.WriteLine("                                  min: ");
+int min= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество чисел в массиве: ");
+void FillArray(int [] collection)
+{
+int lenght = collection.Length;
+int index =0;
+while (index < lenght)
+{
+collection[index] = new Random().Next(min, max);
+index++;
+}
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]+" ");
+        position++;
+    }
+}
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int [a];
+FillArray(array);
+PrintArray(array);
