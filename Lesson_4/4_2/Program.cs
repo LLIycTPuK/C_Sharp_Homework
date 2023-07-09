476dbcc70a018933f001df4e2402e99102b5e8c4
LLIycTPuK <ashliye@yandex.ru>
2023-07-09 14:15:29 +0300

    Classwork Lesson_4

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+double SumNum(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+        all_sum *= i;
+
+return all_sum;
+
+}
+
+int A = int.Parse(Console.ReadLine()!);
+double result = SumNum(A);
+Console.WriteLine(result);
---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int [] collection)
+{
+int lenght = collection.Length;
+int index =0;
+while (index < lenght)
+{
+collection[index] = new Random().Next(0,2);
+index++;
+}
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]+" ");
+        position++;
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
