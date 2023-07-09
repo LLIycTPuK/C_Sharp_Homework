ab107ddc7d3818093de173dfdeb913ffad41b95c
LLIycTPuK <ashliye@yandex.ru>
2023-07-09 14:25:18 +0300

    HomeWork 4_1

---
+++ Lesson_4/HomeWork4/HW4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+double a = double.Parse(Console.ReadLine()!);
+double result = 1;
+Console.WriteLine("Введите степень: ");
+int b= int.Parse(Console.ReadLine()!);
+void PowTable(double num)
+{
+        for (int i = 1; i <= b; i++)
+        {
+            result = result*a;
+            
+        }
+        Console.Write($"{result}");
+}
+PowTable(a);
