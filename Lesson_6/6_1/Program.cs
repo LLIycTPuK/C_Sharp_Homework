52184cb374d75a300caf11e1b2d35af0c8639982
LLIycTPuK <ashliye@yandex.ru>
2023-07-16 16:28:14 +0300

    Home Work 6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine()!);
+
+bool Triangle(int a, int b, int c)
+{
+    if (a < b + c && b < c + a && c < b + a)
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine(Triangle(a, b, c));
---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+
+void Fibonachi(int N)
+{
+    int temp = 0;
+    int temp2 = 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write(temp+ " ");
+        (temp, temp2) = (temp2, temp + temp2);
+    }
+}
+
+Fibonachi(a);
